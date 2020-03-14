f5aab9b18daf0076e45cad632512c50098cd7593
chinmaydash007 <chinmaydash11@gmail.com>
2020-03-14 22:53:44 +0530

    Added new pattern

---
+++ src/Demo/Demo.java
@@ -0,0 +1,16 @@
+package Demo;
+
+import java.math.BigInteger;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(quadraticRoots(120));
+    }
+
+    public static long quadraticRoots(int N) {
+        //Your code here
+        
+    }
+}
+
+
---
+++ src/Demo/Demo.kt
@@ -0,0 +1,6 @@
+package Demo
+
+
+fun main(args: Array<String>) {
+
+}
