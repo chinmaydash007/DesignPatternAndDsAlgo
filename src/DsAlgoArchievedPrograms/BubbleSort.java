58abd4eef11c31bdbf36e8be39ba07b67d60bc2a
chinmaydash007 <chinmaydash11@gmail.com>
2020-04-19 10:49:19 +0530

    Initial Commit

---
+++ src/DsAlgoArchievedPrograms/BubbleSort.java
@@ -0,0 +1,18 @@
+package DsAlgoArchievedPrograms;
+
+public class BubbleSort {
+    int temp;
+
+    public int[] sort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    temp = arr[j - 1];
+                    arr[j - 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+}
--- src/JavaPrograms/Demo.java
+++ src/JavaPrograms/Demo.java
@@ -1,4 +1,20 @@
 package JavaPrograms;
 
+import DsAlgoArchievedPrograms.BubbleSort;
+
 public class Demo {
+    public static void main(String[] args) {
+        int arr[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        BubbleSort bubbleSort = new BubbleSort();
+        printArray(arr);
+        arr = bubbleSort.sort(arr);
+        printArray(arr);
+    }
+
+    static void printArray(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 }
